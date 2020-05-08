210c7283b4cd256f2435adfe442257c715dc99bf
曹伟 <caowei_e@sina.cn>
2020-05-08 16:06:08 +0800

    增加日期工具

---
+++ src/Date/Date.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace Weigot\Tools\Date;
+
+
+class Date
+{
+    /**
+     * 获得本月最后一天日期
+     * @param $time
+     * @param string $format
+     * @return false|string
+     */
+    public static function getMonthLastDate($time, $format = "Y-m-d")
+    {
+        return date($format, strtotime(' +1 month -1 day', $time));// 本月最后一天
+    }
+
+    /**
+     * 上一月日期
+     * @param $time
+     * @param string $format
+     * @return false|string
+     */
+    public static function getPreDate($time, $format = "Y-m-d")
+    {
+        return date($format, strtotime("-1 months", $time));// 上一月日期
+    }
+
+    /**
+     * 获取两个时间之间的月份
+     * @param $sDate
+     * @param $eDate
+     * @param string $format
+     * @return array
+     * @throws \Exception
+     */
+    public static function getDatePeriod($sDate, $eDate, $format = "Y-m")
+    {
+        $result = [];
+        $start = new \DateTime($sDate);
+        $end = new \DateTime($eDate);
+// 时间间距 这里设置的是一个月
+        $interval = \DateInterval::createFromDateString('1 month');
+        $period = new \DatePeriod($start, $interval, $end);
+        foreach ($period as $dt) {
+            $result[] = $dt->format($format);
+        }
+        return $result;
+    }
+}
